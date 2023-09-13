ebda0cde815e9ca92086a1c8ac8a3672dd90bcf7
Hannah <h.dunn@spin.net.au>
2023-09-13 22:24:04 +1000

    actually adds javascript, html and readme info

---
+++ javascript/script.js
@@ -0,0 +1,84 @@
+// const { join } = require("path");
+// what is this??
+
+// morse code dictionary
+const morseCode = {
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+  0: "-----",
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+};
+
+const reverseMorseCode = {};
+for (const key in morseCode) {
+  if (morseCode.hasOwnProperty(key)) {
+    const value = morseCode[key];
+    reverseMorseCode[value] = key;
+  }
+}
+
+const inputField = document.getElementById("input");
+const translateButton = document.getElementById("translate");
+const outputField = document.getElementById("output");
+
+translateButton.addEventListener("click", () => {
+  const inputText = inputField.value.trim().toUpperCase();
+  if (inputField === "") {
+    outputField.textContent = "No Input Provided";
+    return;
+  }
+
+  if (inputText.includes(".")) {
+    const morseWords = inputText.split("/");
+    const translatedWords = morseWords.map((morseWords) => {
+      const morseChars = morseWords.split(" ");
+      return morseChars
+        .map((morseChar) => {
+          return reverseMorseCode[morseChar] || morseChar;
+        })
+        .join("");
+    });
+    outputField.textContent = translatedWords.join(" ");
+  } else {
+    const words = inputText.split(" ");
+    const translatedWords = words.map((word) => {
+      const chars = word.split("");
+      const morseChars = chars.map((char) => {
+        return morseCode[char] || char;
+      });
+      return morseChars.join(" ");
+    });
+    outputField.textContent = translatedWords.join("/");
+  }
+});
---
+++ javascript/script.test.js
@@ -0,0 +1,8 @@
+// testing should have
+// -if empty return from line 58 "No Input Provided";
+// -if its a not allowed character//// or add all the characts if this is hard
+// -it returns words
+// -it returns morse code
+
+//SHOULD ADD
+//info on how translates eg the spaces in the morse code are actually / slashes
