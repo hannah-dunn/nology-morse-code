4a510f202f8ab693ba51c487ccb112104c03dd7f
Hannah <h.dunn@spin.net.au>
2023-09-18 23:08:31 +1000

    adds start of unit testing

--- javascript/script.test.js
+++ javascript/script.test.js
@@ -6,3 +6,39 @@
 
 //SHOULD ADD
 //info on how translates eg the spaces in the morse code are actually / slashes
+
+const { translateToMorse, translateToText } = require("./script");
+
+describe("Morse Code Translation", () => {
+  it("returns the translation of text to morse code", () => {
+    it("Translates a word to Morse code", () => {
+      const text = "HELLO";
+      const expectedMorse = ".... . .-.. .-.. ---";
+      expect(translateToMorse(text)).toBe(expectedMorse);
+    });
+
+    it("Translates a sentence to Morse code", () => {
+      const text = "HELLO WORLD";
+      const expectedMorse = ".... . .-.. .-.. --- / .-- --- .-. .-.. -..";
+      expect(translateToMorse(text)).toBe(expectedMorse);
+    });
+
+    // Add more tests for different cases
+  });
+
+  describe("Morse Code to Text", () => {
+    it("Translates Morse code to a word", () => {
+      const morse = ".... . .-.. .-.. ---";
+      const expectedText = "HELLO";
+      expect(translateToText(morse)).toBe(expectedText);
+    });
+
+    it("Translates Morse code to a sentence", () => {
+      const morse = ".... . .-.. .-.. --- / .-- --- .-. .-.. -..";
+      const expectedText = "HELLO WORLD";
+      expect(translateToText(morse)).toBe(expectedText);
+    });
+
+    // Add more tests for different cases
+  });
+});
