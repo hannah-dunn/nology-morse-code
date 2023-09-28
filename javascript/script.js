1bc140c714cd7e959f5296859b0cef5d6be8b540
Hannah <h.dunn@spin.net.au>
2023-09-28 15:26:10 +1000

    adds split js files

--- javascript/script.js
+++ javascript/script.js
@@ -1,53 +1,15 @@
-// const { join } = require("path");
-// what is this??
+import { morseCode } from "./data";
+import { morseToEnglish } from "./morseToEnglish";
+import { englishToMorse } from "./englishToMorse";
 
-// morse code dictionary
-const morseCode = {
-  A: ".-",
-  B: "-...",
-  C: "-.-.",
-  D: "-..",
-  E: ".",
-  F: "..-.",
-  G: "--.",
-  H: "....",
-  I: "..",
-  J: ".---",
-  K: "-.-",
-  L: ".-..",
-  M: "--",
-  N: "-.",
-  O: "---",
-  P: ".--.",
-  Q: "--.-",
-  R: ".-.",
-  S: "...",
-  T: "-",
-  U: "..-",
-  V: "...-",
-  W: ".--",
-  X: "-..-",
-  Y: "-.--",
-  Z: "--..",
-  0: "-----",
-  1: ".----",
-  2: "..---",
-  3: "...--",
-  4: "....-",
-  5: ".....",
-  6: "-....",
-  7: "--...",
-  8: "---..",
-  9: "----.",
-};
-
-const reverseMorseCode = {};
-for (const key in morseCode) {
-  if (morseCode.hasOwnProperty(key)) {
-    const value = morseCode[key];
-    reverseMorseCode[value] = key;
+const reverseMorseCode = () => {
+  for (const key in morseCode) {
+    if (morseCode.hasOwnProperty(key)) {
+      const value = morseCode[key];
+      reverseMorseCode[value] = key;
+    }
   }
-}
+};
 
 const inputField = document.getElementById("input");
 const translateButton = document.getElementById("translate");
@@ -56,34 +18,30 @@ const outputField = document.getElementById("output");
 const noInput = new Error("No Input Provided");
 const cantRead = new Error("Cannot translate special characters(e.g !@#$%)");
 
-translateButton.addEventListener("click", () => {
-  const inputText = inputField.value.trim().toUpperCase();
-  if (inputField === "") {
+const validateString = (string) => {
+  if (string === "") {
     throw noInput;
-    // outputField.textContent = "No Input Provided";
-    // return;
   }
+  if (!/^[a-zA-Z]*$/.test(string)) {
+    throw cantRead;
+  }
+  return string;
+};
 
-  if (inputText.includes(".")) {
-    const morseWords = inputText.split("/");
-    const translatedWords = morseWords.map((morseWords) => {
-      const morseChars = morseWords.split(" ");
-      return morseChars
-        .map((morseChar) => {
-          return reverseMorseCode[morseChar] || morseChar;
-        })
-        .join("");
-    });
-    outputField.textContent = translatedWords.join(" ");
-  } else {
-    const words = inputText.split(" ");
-    const translatedWords = words.map((word) => {
-      const chars = word.split("");
-      const morseChars = chars.map((char) => {
-        return morseCode[char] || char;
-      });
-      return morseChars.join(" ");
-    });
-    outputField.textContent = translatedWords.join("/");
+translateButton.addEventListener("click", () => {
+  try {
+    const str = validateInput(inputText);
+    if (isMorse(str)) {
+      const translation = morseToEnglish(str, obj);
+      outputField.textContent = translation;
+    } else {
+      const translation = englishToMorse(str, reversedObj);
+      outputField.textContent = translation;
+    }
+  } catch (e) {
+    // add the error to the dom
   }
 });
+
+const isMorse = (string) => string.includes(".");
+outputField.textContent = translatedWords.join(" ");
--- javascript/script.test.js
+++ javascript/script.test.js
@@ -1,50 +1,11 @@
-// testing should have
-// -if empty return from line 58 "No Input Provided";
-// -if its a not allowed character//// or add all the characts if this is hard
-// -it returns words
-// -it returns morse code
-
-const {
-  translateToMorse,
-  translateToText,
-  noInput,
-  cantRead,
-} = require("./script");
+import { noInput, cantRead } from "./script";
 
 describe("morse code tests", () => {
-  it("returns the translation of text to morse code", () => {
-    it("translates a word to morse code", () => {
-      const text = "hello";
-      const expectedMorse = ".... . .-.. .-.. ---";
-      expect(translateToMorse(text)).toBe(expectedMorse);
-    });
-
-    it("translates a sentence to morse code", () => {
-      const text = "hello world";
-      const expectedMorse = ".... . .-.. .-.. --- / .-- --- .-. .-.. -..";
-      expect(translateToMorse(text)).toBe(expectedMorse);
-    });
-
-    it("Should throw an error if textarea empty", () => {
-      expect(inputField === "").toThrow(noInput);
-    });
-
-    it("should throw an error if special characters are used", () => {
-      expect(inputField === "/W/").toThrow(cantRead);
-    });
+  it("Should throw an error if textarea empty", () => {
+    expect(inputField === "").toThrow(noInput);
   });
 
-  describe("morse code to text", () => {
-    it("returns morse code as text", () => {
-      const morse = ".... . .-.. .-.. ---";
-      const expectedText = "HELLO";
-      expect(translateToText(morse)).toBe(expectedText);
-    });
-
-    it("returns morse code as a sentence", () => {
-      const morse = ".... . .-.. .-.. --- / .-- --- .-. .-.. -..";
-      const expectedText = "HELLO WORLD";
-      expect(translateToText(morse)).toBe(expectedText);
-    });
+  it("should throw an error if special characters are used", () => {
+    expect(inputField === "/W/").toThrow(cantRead);
   });
 });
